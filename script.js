401d13d6061bb38c3e1aa1ea269f4cf8248e58af
gitauwilly1 <gitauwilly254@gmail.com>
2026-03-01 14:13:16 +0300

    add script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const calorieForm = document.getElementById('calorieForm');
+const foodItemInput = document.getElementById('foodItem');
+const calorieAmountInput = document.getElementById('calorieAmount');
+const foodList = document.getElementById('foodList');
+const totalCaloriesDisplay = document.getElementById('totalCalories');
+const resetBtn = document.getElementById('resetBtn');
+
+let calorieEntries = [];
